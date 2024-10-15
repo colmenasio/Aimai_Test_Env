5ab1b349abb8e1e149bb3e69ddd9cf24e524a73d
colmenasio <a.colmenero.fernandez@gmail.com>
2024-10-16 00:22:38 +0200

    Readibility changes

--- common/shmem_channel/shmem_channel.cpp
+++ common/shmem_channel/shmem_channel.cpp
@@ -3,41 +3,36 @@
 template <typename T>
 shmem_channel<T>::shmem_channel(int channel, SHMEM_RIGHTS mode) : mode(mode)
 {
-    bool shmem_needs_initialization;
-    // Get the key
+    // Generate a key
     this->shm_key = ftok("shmfile", channel);
 
-    // Check if shared memory exists or create it if necessary
-
-    this->shm_id = shmget(this->shm_key, sizeof(data_frame<T>), 0666 | IPC_CREAT | IPC_EXCL);
-    if (this->shm_id == -1)
+    // Check if shared memory associated with this key exists or it we need to create it
+    bool shmem_creation_succeded = (shmget(this->shm_key, sizeof(data_frame<T>), 0666 | IPC_CREAT | IPC_EXCL) != -1);
+    bool shmem_already_existent = (errno == EEXIST);
+    if (shmem_creation_succeded)
     {
-        if (errno == EEXIST)
-        {
-            // The memory segment already exists.
-            this->shm_id = shmget(this->shm_key, sizeof(data_frame<T>), 0666);
-            shmem_needs_initialization = false;
-        }
-        else
-        {
-            std::cout << "Error creating shared mem";
-        }
+        this->shm_id = shmget(this->shm_key, sizeof(data_frame<T>), 0666) /* The creation succeded so we can get the id */
     }
-    else
+    else if (!shmem_creation_succeded && shmem_already_existent)
     {
-        // The memory segment was just created. Initialize it
-        shmem_needs_initialization = true;
+        this->shm_id = shmget(this->shm_key, sizeof(data_frame<T>), 0666) /* The creation failed cause the shm segment already exists */
+    }
+    else if (!shmem_creation_succeded && !shmem_already_existent) /* The creation failed cause of unknonw reasons. Except and do cleanup */
+    {
+        // Here there should be cleanup and exceptions or something
+        std::cout << "Error creating shared mem";
     }
 
     // Attach to the shared memory
     this->shm_data = (data_frame<T> *)shmat(this->shm_id, nullptr, 0);
     if (this->shm_data == (data_frame<T> *)(-1))
     {
+        // Here there should be cleanup and exceptions or something
         std::cout << "Couldn't attach to shared memory";
     }
 
     // Initalize if needed
-    if (shmem_needs_initialization)
+    if (shmem_creation_succeded) /* If creation succeded, it means it was just created and we need to initialize */
     {
         memset(this->shm_data, 0, sizeof(data_frame<T>));
     }
@@ -57,7 +52,7 @@ shmem_channel<T>::~shmem_channel()
 }
 
 template <typename T>
-T* shmem_channel<T>::get_shmem_data_accesor()
+T *shmem_channel<T>::get_shmem_data_accesor()
 {
     return &(this->shm_data->data);
 }
