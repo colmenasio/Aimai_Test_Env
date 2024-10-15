59319692832e414e0da6ab5e42a6d36e12ae3e41
colmenasio <a.colmenero.fernandez@gmail.com>
2024-10-16 01:12:52 +0200

    Minor changes

---
+++ common/common_structs/common_structs.hpp
@@ -0,0 +1,19 @@
+#ifndef COMMON_STRUCTS_HPP
+#define COMMON_STRUCTS_HPP
+
+namespace common_structs{
+    struct RRCAR_COMMAND{
+        double gas;
+        double brake;
+        double steer; // Counter-clockwise horizontal rotation with origin in the z-axis 
+    };
+    struct RRCAR_POSITION
+    {
+        double xpos;
+        double zpos;
+        double angle; // Counter-clockwise horizontal rotation with origin in the z-axis 
+    };
+    
+}
+
+#endif
